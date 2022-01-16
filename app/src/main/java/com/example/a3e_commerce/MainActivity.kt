65a6a97fa724d994574342c6f77e33ebb8e6a7a7
Yasser Baidi <yasserbd181@gmail.com>
2022-01-16 22:28:48 +0100

    e-commerce app android 16/01/2022 last

--- app/src/main/java/com/example/a3e_commerce/MainActivity.kt
+++ app/src/main/java/com/example/a3e_commerce/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.a3e_commerce
 
 import android.os.Bundle
+import android.util.Log.d
+import android.view.MenuItem
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.a3e_commerce.model.Product
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.main.*
 
@@ -15,6 +19,20 @@ class MainActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
             setSupportActionBar(toolbar)
+            navigationView.setNavigationItemSelectedListener {
+                it.isChecked=true
+                drawerLayout.closeDrawers()
+                when (it.itemId){
+                    R.id.action_home->d("yasalam","Home Page")
+                    R.id.action_hoodie->d("yasalam","Buy Hoodie")
+                    R.id.action_shoes->d("yasalam","Buy shoes")
+                }
+                true
+            }
+            supportActionBar?.apply {
+                setDisplayHomeAsUpEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+            }
 
             val products = arrayListOf<Product>()
             for ( i in 0..10){
@@ -25,5 +43,11 @@ class MainActivity : AppCompatActivity() {
                 layoutManager= GridLayoutManager(this@MainActivity,2)
                 adapter= ProductAdapter(products)
             }
+
+        }
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            drawerLayout.openDrawer(GravityCompat.START)
+            return true
+           // return super.onOptionsItemSelected(item)
         }
     }
